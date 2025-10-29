361a29271f836c84f08f0496f10006c18fac83dc
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-10-29 13:50:30 -0600

    e agregó el componente de Presupuesto y se integró a App.vue

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <form>
+
+    <div class="campo">
+      <label for="">Definir Presupuesto</label>
+
+      <input
+        id="nuevo-presupuesto"
+        class="nuevo-presupuesto"
+        placeholder="Añade tu presupuesto"
+      />
+    </div>
+
+    <input type="submit" value="Definir Presupuesto" />
+  </form>
+</template>
