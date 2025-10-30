59f7755df5acfcc2611d4de1c3cd1c5c69716580
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-10-30 12:20:12 -0600

    Se agregó el componente de Presupuesto con validación e integración de Alertas para los mensajes de error

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,21 +1,35 @@
 <script setup>
-  import { ref } from "vue";
+import { ref } from "vue";
+import Alerta from "./Alerta.vue";
 
-  const presupuesto = ref(0)
+const presupuesto = ref(0);
+const error = ref('')
 
-  const definirPresupuesto = () => {
-    if (presupuesto.value <= 0) {
-      console.log('Presupuesto no válido.')
-    }
+const emmit = defineEmits(['definir-presupuesto'])
+
+const definirPresupuesto = () => {
+  if (presupuesto.value <= 0 || presupuesto.value === '') {
+        error.value = 'Presupuesto no válido'
+
+        setTimeout(() => {
+            error.value = ''
+        }, 2500);
+
+        return
   }
 
+  emmit('definir-presupuesto', presupuesto.value)
+};
 </script>
 
 <template>
-  <form
-        class="presupuesto"
-        @submit.prevent="definirPresupuesto"
-  >
+  <form class="presupuesto" @submit.prevent="definirPresupuesto">
+
+    <Alerta
+      v-if="error"
+    >
+      {{ error }}
+    </Alerta>
 
     <div class="campo">
       <label for="nuevo-presupuesto">Definir Presupuesto</label>
@@ -35,40 +49,45 @@
 </template>
 
 <style scoped>
-  .presupuesto {
-    width: 100%;
-  }
-  .campo{
-    display: grid;
-    gap: 2rem;
-  }
-  .presupuesto label{
-    font-size: 2.2rem;
-    text-align: center;
-    color: var(--azul);
-  }
-  .presupuesto input[type="number"]{
-    background-color: var(--gris-claro);
-    border-radius: 1rem;
-    padding: 1rem;
-    border: none;
-    font-size: 2.2rem;
-    text-align: center;
-  }
-  .presupuesto input[type="submit"]{
-    background-color: var(--azul);
-    border: none;
-    padding: 1rem;
-    text-align: center;
-    font-size: 2rem;
-    margin-top: 2rem;
-    color: var(--blanco);
-    font-weight: 900;
-    width: 100%;
-    transition: background-color 300ms ease;
-  }
-    .presupuesto input[type="submit"]:hover{
-    background-color: #1048A4;
-    cursor: pointer;
-  }
-  </style>
+.presupuesto {
+  width: 100%;
+}
+
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+
+.presupuesto label {
+  font-size: 2.2rem;
+  text-align: center;
+  color: var(--azul);
+}
+
+.presupuesto input[type="number"] {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+  text-align: center;
+}
+
+.presupuesto input[type="submit"] {
+  background-color: var(--azul);
+  border: none;
+  padding: 1rem;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+  color: var(--blanco);
+  font-weight: 900;
+  width: 100%;
+  transition: background-color 300ms ease;
+}
+
+.presupuesto input[type="submit"]:hover {
+  background-color: #1048a4;
+  cursor: pointer;
+}
+</style>
