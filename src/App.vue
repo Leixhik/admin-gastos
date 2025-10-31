259d1897c73a2b69507d5d79871870d5eff0ca31
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-10-31 13:09:00 -0600

    Se añadió el componente Modal y se implementó la funcionalidad para mostrarlo al hacer clic en el icono de nuevo gasto en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
-  import { ref } from "vue";
+  import { ref, reactive } from "vue";
   import Presupuesto from "./components/Presupuesto.vue";
   import ControlPresupuesto from "./components/ControlPresupuesto.vue";
+  import Modal from './components/Modal.vue'
+  import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
+  const modal = reactive({
+    mostrar: false,
+    animar: false
+  })
   const presupuesto = ref(0)
   const disponible = ref(0)
 
   const definirPresupuesto = (cantidad) => {
     presupuesto.value = cantidad
     disponible.value = cantidad
+  }
+
+  const mostrarModal = () => {
+    modal.mostrar = true
+    modal.animar = true
   }
 </script>
 
@@ -31,6 +42,22 @@
 
       </div>
     </header>
+
+    <main v-if="presupuesto > 0">
+
+      <div class="crear-gasto">
+          <img
+            :src="iconoNuevoGasto"
+            alt="icono nuevo gasto"
+            @click="mostrarModal"
+          />
+      </div>
+
+      <Modal
+          v-if="modal.mostrar"
+      />
+    </main>
+
   </div>
 </template>
 
@@ -88,4 +115,15 @@
     border-radius: 1.2rem;
     padding: 5rem;
   }
+
+  .crear-gasto{
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+  }
+  .crear-gasto img{
+    width: 5rem;
+    cursor: pointer;
+  }
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import cerrarModal from '../assets/img/cerrar.svg'
+</script>
+
+<template>
+  <div class="modal">
+      <div class="cerrar-modal">
+          <img
+              :src="cerrarModal"
+              alt=""
+            />
+      </div>
+  </div>
+</template>
+
+<style scoped>
+  .modal {
+    position: absolute;
+    background-color: rgb(0 0 0 / 0.9);
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
