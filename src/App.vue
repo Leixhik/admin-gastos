f9ebe31b900d0d17d8252c06c5e05eb3a367e5c2
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-11-03 14:13:26 -0600

    Se mejoró la animación del modal al mostrar y ocultar, y se integraron las propiedades del modal en el componente Modal.vue

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,16 @@
 
   const mostrarModal = () => {
     modal.mostrar = true
-    modal.animar = true
+    setTimeout(() => {
+      modal.animar = true
+    }, 300);
   }
 
   const ocultarModal = () => {
-    modal.mostrar = false
     modal.animar = false
+    setTimeout(() => {
+      modal.mostrar = false
+    }, 300);
   }
 </script>
 
@@ -61,6 +65,7 @@
       <Modal
           v-if="modal.mostrar"
           @ocultar-modal="ocultarModal"
+          :modal="modal"
       />
     </main>
 
