a18c7ff702da78afed89c1c8268d547beb59a19e
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-11-21 13:44:18 -0600

    Se agregó vue3 circle progress para la barra estadística del componente ControlPresupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,6 @@
 <script setup>
-import imagen from "../assets/img/grafico.jpg";
+import CircleProgress from 'vue3-circle-progress'
+import "vue3-circle-progress/dist/circle-progress.css"
 import { formatearCantidad } from '../helpers'
 
 const emit = defineEmits(['reset-app'])
@@ -23,7 +24,14 @@ const props = defineProps({
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img :src="imagen" />
+      <CircleProgress
+        :percent="50"
+        :size="250"
+        :border-with="20"
+        :border-bg-width="15"
+        fill-color="#3b82f6"
+        empty-color="#e1e1e1"
+      />
     </div>
 
     <div class="contenedor-presupuesto">
