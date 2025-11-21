9816feb2afa79e85cf32089f45e1921cc6bf770c
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-11-21 13:56:43 -0600

    FINALIZADO
    Se terminó el proyecto del código, a partir de aquí me gustaría darle más funcionalidades y mejoras por mi cuenta

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from 'vue'
 import CircleProgress from 'vue3-circle-progress'
 import "vue3-circle-progress/dist/circle-progress.css"
 import { formatearCantidad } from '../helpers'
@@ -19,13 +20,20 @@ const props = defineProps({
     required: true
   }
 })
+
+const porcentaje = computed(() =>{
+  return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+})
 </script>
 
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
+
+      <p class="porcentaje">{{ porcentaje }}%</p>
+
       <CircleProgress
-        :percent="50"
+        :percent="porcentaje"
         :size="250"
         :border-with="20"
         :border-bg-width="15"
@@ -62,9 +70,24 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.contenedor-grafico{
+  position: relative;
+}
 .dos-columnas {
   display: flex;
   flex-direction: column;
+}
+.porcentaje {
+  position: absolute;
+  margin: auto;
+  top: calc(50% - 1.5rem);
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 100;
+  font-size: 3rem;
+  font-weight: 900;
+  color: var(--gris-oscuro);
 }
 .dos-columnas > :first-child {
   margin-bottom: 3rem;
